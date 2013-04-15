a914b5e52574059a5421ed47463f95391a3d1928
Brian Shirai <brixen@gmail.com>
2013-04-14 17:14:51 -0700

    Created gem.

---
+++ lib/rubysl-open-uri.rb
@@ -0,0 +1,9 @@
+require "rubysl-open-uri/version"
+
+module RubySL
+  module Open
+    module Uri
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rubysl-open-uri/version.rb
@@ -0,0 +1,7 @@
+module RubySL
+  module Open
+    module Uri
+      VERSION = "0.0.1"
+    end
+  end
+end
