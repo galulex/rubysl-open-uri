9f87f56c4b8cd0882b2ee321b5f829b505ca592e
Brian Shirai <brixen@gmail.com>
2013-09-05 10:53:43 -0700

    Fixed gem file structure.

---
+++ lib/rubysl/open-uri/version.rb
@@ -0,0 +1,5 @@
+module RubySL
+  module OpenURI
+    VERSION = "0.0.1"
+  end
+end
--- lib/rubysl/open/uri.rb
+++
@@ -1,9 +0,0 @@
-require "rubysl-open-uri/version"
-
-module RubySL
-  module Open
-    module Uri
-      # Your code goes here...
-    end
-  end
-end
